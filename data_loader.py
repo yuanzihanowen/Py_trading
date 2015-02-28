3e44eaaec4512754fedda8adccb267ba175402ed
Owen <zihan.yuan.owen@gmail.com>
2015-02-28 14:28:58 +0800

    Update and rename function_trading.py to data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,6 +2,9 @@ __author__ = 'zihanyuanowen'
 
 import requests
 import json
+import pandas as pd
+# import numpy as np
+# import logging
 
 
 def connect(domain, access_token, account_id, inst):
@@ -25,9 +28,13 @@ def connect(domain, access_token, account_id, inst):
 
 def read_data(r, inst_name):
     json_file = r.json()  # r is the response
-    file_name = inst_name + '.txt'
+    file_name = inst_name + '.json'
     with open(file_name, "w") as outfile:
         json.dump(json_file, outfile)
+
+    json_data = json.load(open(file_name))
+    df_dict_instrument = pd.DataFrame(json_data['candles'])
+    df_dict_instrument.to_csv(inst_name+'_data.csv')
 
 
 def load_dict(accountid, dict_domain, token):
@@ -38,15 +45,23 @@ def load_dict(accountid, dict_domain, token):
     req = requests.Request('GET', dict_domain, headers=hdr, params=parameter)
     pre = req.prepare()
     dict_resp = s.send(pre)
-    with open('dict.txt', "w") as outfile:
+    with open('dict.json', "w") as outfile:
         json.dump(dict_resp.json(), outfile)
+
+    json_data = open('dict.json')
+    data = json.load(json_data)
+    list_instruments = data["instruments"]
+    df_dict_instrument = pd.DataFrame(list_instruments)
+    df_dict_instrument.to_csv('dict_instr.csv')
+    return df_dict_instrument
 
 
 if __name__ == '__main__':
     dm = 'https://api-fxpractice.oanda.com'
     a_t = '9e6898d3bc4f5ef5dd0cacd8fd025b31-7dc39a42131d5dfbaa3844e0ea330914' #Access Token needed
     acct_id = '8244239'
-    instrument = 'EUR_USD'
-    load_dict(acct_id, dm, a_t)
-    resp = connect(dm, a_t, acct_id, instrument)
-    read_data(resp, instrument)
+
+    dict_security = load_dict(acct_id, dm, a_t)
+    for instrument in dict_security['instrument']:
+        resp = connect(dm, a_t, acct_id, instrument)
+        read_data(resp, instrument)
