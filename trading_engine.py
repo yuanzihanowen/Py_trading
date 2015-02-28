caf2465b0c01d222a4a0e4e879b01166a530db17
Owen <zihan.yuan.owen@gmail.com>
2015-02-28 19:57:51 +0800

    Create trading_engine.py
    calculation of cci index

---
+++ trading_engine.py
@@ -0,0 +1,35 @@
+__author__ = 'zihanyuanowen'
+
+import requests
+import json
+import pandas as pd
+import numpy as np
+import os
+
+
+def calculate_cci(security_name, period=15):
+    px_file_path = os.getcwd() + '/output/' + security_name + '_data.csv'
+    df = pd.read_csv(px_file_path)
+
+    df_tp = pd.DataFrame(df, columns=['lowAsk', 'highAsk', 'closeAsk', 'time'])
+    df_tp = df_tp.set_index('time')
+    df_ask_last = pd.DataFrame(df_tp, columns=['closeAsk'])
+    df_ask_high = pd.DataFrame(df_tp, columns=['highAsk'])
+    df_ask_low = pd.DataFrame(df_tp, columns=['lowAsk'])
+    df_tp = df_tp.sum(1)/3
+    df_sma_tp = pd.rolling_mean(df_tp, window=period)
+    df_md = pd.rolling_mean(df_sma_tp, window=period)
+    df_md = abs(df_tp - df_md)*0.015
+    df_cci = (df_tp - df_sma_tp)/df_md
+    df_cci.to_csv(os.getcwd() +'/engine_output/' + security_name + '_cci.csv')
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.expanduser('~/Desktop/PyScrap'))
+    # security_list = ['EUR_USD', 'USD_JPY', 'GBP_USD', 'USD_CHF']
+    security_list = ['EUR_USD']
+
+    for security in security_list:
+        if not os.path.exists(os.getcwd() +'/engine_output/'):
+            os.makedirs(os.getcwd() +'/engine_output/')
+        calculate_cci(security)
